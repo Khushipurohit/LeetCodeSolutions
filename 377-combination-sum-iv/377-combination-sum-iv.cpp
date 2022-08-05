4f1f410113a86a299c705a8fbf26e59ece57cb43
Khushipurohit <87742828+Khushipurohit@users.noreply.github.com>
2022-08-06 02:46:52 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (91.95%) - LeetHub

--- 377-combination-sum-iv/377-combination-sum-iv.cpp
+++ 377-combination-sum-iv/377-combination-sum-iv.cpp
@@ -3,8 +3,7 @@ public:
    
     int solve(vector<int>& nums, int target, vector<int>& dp){
     int res = 0;
-        //vector<int> dp(target + 1, -1);
-        //dp[0] = 1;
+       
         if(target < 0){
             return 0;
         }
@@ -16,14 +15,12 @@ public:
         if(dp[target] != -1){
             return dp[target];
         }
-       // for(int i = 1; i <= target; i++){
+      
         for(int i = 0; i < nums.size(); i++){
-            // if(i - nums[j] >= 0){
-            // dp[i] += dp[i - nums[j]];
-            // }
+            
             res += solve(nums, target - nums[i], dp);
         }
-       // }
+      
         dp[target] = res;
         
         return dp[target];
