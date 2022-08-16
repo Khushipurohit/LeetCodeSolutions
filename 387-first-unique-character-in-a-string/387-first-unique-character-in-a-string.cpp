91dde0c8ed995cea86542aefd55d0712dc49368e
Khushipurohit <87742828+Khushipurohit@users.noreply.github.com>
2022-08-16 13:42:22 +0530

    Time: 99 ms (14.34%), Space: 10.9 MB (15.57%) - LeetHub

--- 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.cpp
+++ 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.cpp
@@ -3,34 +3,6 @@ public:
     int firstUniqChar(string s) {
         map<char, int>m;
         
-//         for(int i = 0; i < s.length(); i++){
-//             if(m.find(s[i]) == m.end()){
-//                 m.insert(make_pair(s[i], 1));
-//             }
-            
-//             else{
-//                 m[s[i]]++;
-//             }
-//         }
-//         char k = '0';
-//         for(auto& it : m){
-            
-//             // if(it.second == 1){
-//             //     k = it.first;
-//             //     break;
-//             // }
-            
-//             cout << it.first << " " << it.second << endl;
-//         }
-//         //cout << k << endl;
-//        for(int i = 0; i < s.length(); i++){
-//            if(k == s[i]){
-//                return i;
-//            }
-//        }
-        
-//         return -1;
-        
         for(char i : s){
             m[i]++;
         }
