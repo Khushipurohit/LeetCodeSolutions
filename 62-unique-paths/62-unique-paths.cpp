442b4e9a3c2d3234987136142cb08a1c78c336ef
Khushipurohit <87742828+Khushipurohit@users.noreply.github.com>
2022-09-13 19:22:17 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (98.66%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -2,8 +2,7 @@ class Solution {
 public:
     int uniquePaths(int m, int n) {
         
-        // int k = m+n-2;
-        // int l = m-1;
+        
         double ans=1;
         
         for(int i = 1; i < m; i++)
